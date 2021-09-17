653abe37849f5fb7572c8ec128340c8855524284
Sam.Hsu <Sam.Hsu@advantech.com.tw>
2021-09-17 10:03:06 +0800

    feat: Add a rule for high water level
    Signed-off-by: Sam.Hsu <Sam.Hsu@advantech.com.tw>

--- ruleengine/create_rules.sh
+++ ruleengine/create_rules.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-waterLevelThreshold=20
+lowWaterLevelThreshold=20
+highWaterLevelThreshold=50
 
-declare -a ruleName=("pump_control" "person_detection" "water_level_alert" "person_detection_alert")
+declare -a ruleName=("pump_control_on" "pump_control_off" "person_detection" "water_level_alert" "person_detection_alert")
 
 delete_rules(){
     echo
@@ -27,8 +28,8 @@ create_rules(){
     -d '{"sql": "create stream demo() WITH (FORMAT=\"JSON\", TYPE=\"edgex\")"}'
     echo
 
-    echo "Create the new rules for pump control."
-    echo "The rule will be triggered if the waterLevelThreshold <= "$1
+    echo "Create the new rules for pump control of low water level."
+    echo "The rule will be triggered if the lowWaterLevelThreshold <= "$1
     curl -X POST \
     http://172.17.0.1:48075/rules \
     -H 'Content-Type: application/json' \
@@ -40,7 +41,7 @@ create_rules(){
         "rest": {
             "url": "http://172.17.0.1:49991/api/v1/device/name/FATEKPLC/Pump",
             "method": "put",
-            "dataTemplate": "{\"Pump\":\"true\"}",
+            "dataTemplate": "{\"Pump\":\"1\"}",
             "sendSingle": true
         },
         "mqtt": {
@@ -56,19 +57,20 @@ create_rules(){
     }'
     echo
 
-    echo "Create the new rules for person detection."
+    echo "Create the new rules for pump control of high water level."
+    echo "The rule will be triggered if the highWaterLevelThreshold >= "$2
     curl -X POST \
     http://172.17.0.1:48075/rules \
     -H 'Content-Type: application/json' \
     -d '{
     "id": "'${ruleName[1]}'",
-    "sql": "SELECT rand() as rkey, json as base64 FROM demo WHERE meta(device) = \"sample-json\" GROUP BY SLIDINGWINDOW(ss, 5) HAVING COUNT(*) = 1",
+    "sql": "SELECT WaterLevel FROM demo WHERE meta(device) = \"WISE-4012\" AND WaterLevel >= '$2'",
     "actions": [
         {
         "rest": {
-            "url": "http://172.17.0.1:48060/api/v1/notification",
-            "method": "post",
-            "dataTemplate": "{\"slug\":\"edgex-admin-{{.rkey}}\",\"sender\": \"System Management\",\"category\": \"SECURITY\",\"severity\": \"CRITICAL\",\"contentType\": \"text/html\",\"content\": \"You have to take action for this alert:<br><img src=data:image/jpeg;base64,{{.base64}} />\",\"labels\": [\"PersonDetection\"]}",
+            "url": "http://172.17.0.1:49991/api/v1/device/name/FATEKPLC/Pump",
+            "method": "put",
+            "dataTemplate": "{\"Pump\":\"0\"}",
             "sendSingle": true
         },
         "mqtt": {
@@ -82,21 +84,21 @@ create_rules(){
         }
     ]
     }'
-    echo
+    echo    
 
-    echo "Create the new rules for water level alert."
+    echo "Create the new rules for person detection."
     curl -X POST \
     http://172.17.0.1:48075/rules \
     -H 'Content-Type: application/json' \
     -d '{
     "id": "'${ruleName[2]}'",
-    "sql": "SELECT WaterLevel FROM demo WHERE meta(device) = \"WISE-4012\" AND WaterLevel <= '$1' GROUP BY SLIDINGWINDOW(ss, 5) HAVING COUNT(*) = 1",
+    "sql": "SELECT rand() as rkey, json as base64 FROM demo WHERE meta(device) = \"sample-json\" GROUP BY SLIDINGWINDOW(ss, 5) HAVING COUNT(*) = 1",
     "actions": [
         {
         "rest": {
-            "url": "http://172.17.0.1:48080/api/v1/event",
+            "url": "http://172.17.0.1:48060/api/v1/notification",
             "method": "post",
-            "dataTemplate": "{\"device\":\"Alert\",\"readings\":[{\"device\":\"Alert\",\"name\":\"Message_ID\",\"value\":\"2\"}]}",
+            "dataTemplate": "{\"slug\":\"edgex-admin-{{.rkey}}\",\"sender\": \"System Management\",\"category\": \"SECURITY\",\"severity\": \"CRITICAL\",\"contentType\": \"text/html\",\"content\": \"You have to take action for this alert:<br><img src=data:image/jpeg;base64,{{.base64}} />\",\"labels\": [\"PersonDetection\"]}",
             "sendSingle": true
         },
         "mqtt": {
@@ -112,19 +114,19 @@ create_rules(){
     }'
     echo
 
-    echo "Create the new rules for person detection alert."
+    echo "Create the new rules for water level alert."
     curl -X POST \
     http://172.17.0.1:48075/rules \
     -H 'Content-Type: application/json' \
     -d '{
     "id": "'${ruleName[3]}'",
-    "sql": "SELECT AnalyticsData FROM demo WHERE meta(device) = \"MQTTAnalyticservice\" GROUP BY SLIDINGWINDOW(ss, 5) HAVING COUNT(*) = 1",
+    "sql": "SELECT WaterLevel FROM demo WHERE meta(device) = \"WISE-4012\" AND WaterLevel <= '$1' GROUP BY SLIDINGWINDOW(ss, 5) HAVING COUNT(*) = 1",
     "actions": [
         {
         "rest": {
             "url": "http://172.17.0.1:48080/api/v1/event",
             "method": "post",
-            "dataTemplate": "{\"device\":\"Alert\",\"readings\":[{\"device\":\"Alert\",\"name\":\"Message_ID\",\"value\":\"1\"}]}",
+            "dataTemplate": "{\"device\":\"Alert\",\"readings\":[{\"device\":\"Alert\",\"name\":\"Message_ID\",\"value\":\"2\"}]}",
             "sendSingle": true
         },
         "mqtt": {
@@ -139,7 +141,35 @@ create_rules(){
     ]
     }'
     echo
+
+    echo "Create the new rules for person detection alert."
+    curl -X POST \
+    http://172.17.0.1:48075/rules \
+    -H 'Content-Type: application/json' \
+    -d '{
+    "id": "'${ruleName[4]}'",
+    "sql": "SELECT AnalyticsData FROM demo WHERE meta(device) = \"MQTTAnalyticservice\" GROUP BY SLIDINGWINDOW(ss, 5) HAVING COUNT(*) = 1",
+    "actions": [
+        {
+        "rest": {
+            "url": "http://172.17.0.1:48080/api/v1/event",
+            "method": "post",
+            "dataTemplate": "{\"device\":\"Alert\",\"readings\":[{\"device\":\"Alert\",\"name\":\"Message_ID\",\"value\":\"1\"}]}",
+            "sendSingle": true
+        },
+        "mqtt": {
+            "server": "172.17.0.1:1883",
+            "topic": "edgex/'${ruleName[4]}'",
+            "sendSingle": true
+        }
+        },
+        {
+        "log":{}
+        }
+    ]
+    }'
+    echo
 }
 
 delete_rules
-create_rules $waterLevelThreshold
+create_rules $lowWaterLevelThreshold $highWaterLevelThreshold
